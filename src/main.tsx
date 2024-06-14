f7de8eb2b68ea134eed252a9a1a43a12ed2d00f5
chriscors <70537376+chriscors@users.noreply.github.com>
2024-06-14 14:29:53 -0400

    Relocate MantineProvider

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
+import { MantineProvider } from "@mantine/core";
+import { theme } from "./theme.ts";
+import "@mantine/core/styles.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <MantineProvider theme={theme}>
+      <App />
+    </MantineProvider>
   </React.StrictMode>
 );
