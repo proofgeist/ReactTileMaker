c57c2056c7df0ece32a3a7e1bb7f70f333aa2fb7
chriscors <70537376+chriscors@users.noreply.github.com>
2024-06-14 14:29:34 -0400

    Add Appshell structure
    Co-authored-by: pvino73 <pvino73@users.noreply.github.com>
    Co-authored-by: csigfrinius <csigfrinius@users.noreply.github.com>
    Co-authored-by: bcooney93 <bcooney93@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,29 @@
+import { AppShell, Burger, Group, Skeleton } from "@mantine/core";
 import "@mantine/core/styles.css";
-import { MantineProvider } from "@mantine/core";
-import { theme } from "./theme";
+import { useDisclosure } from "@mantine/hooks";
 
 export default function App() {
-  return <MantineProvider theme={theme}>App</MantineProvider>;
+  const [opened, { toggle }] = useDisclosure();
+  return (
+    <AppShell
+      header={{ height: 60 }}
+      navbar={{ width: 300, breakpoint: "sm", collapsed: { mobile: !opened } }}
+      padding="md"
+    >
+      <AppShell.Header>
+        <Group h="100%" px="md">
+          <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
+        </Group>
+      </AppShell.Header>
+      <AppShell.Navbar p="md">
+        Navbar
+        {Array(15)
+          .fill(0)
+          .map((_, index) => (
+            <Skeleton key={index} h={28} mt="sm" animate={false} />
+          ))}
+      </AppShell.Navbar>
+      <AppShell.Main>Main</AppShell.Main>
+    </AppShell>
+  );
 }
