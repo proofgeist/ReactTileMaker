097fb6428d37fec43784ea177a42b2db94497fb5
proof-pv <peter.vinogradov@proofgeist.com>
2024-06-28 13:28:38 -0400

    Cleared the header added body content

--- src/App.tsx
+++ src/App.tsx
@@ -36,8 +36,6 @@ export default function App() {
       padding="md"
     >
       <AppShell.Header>
-        <h1>DIAMIND</h1>
-        <h1>circle</h1>
         <Group h="100%" px="md">
           <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
         </Group>
@@ -57,7 +55,7 @@ export default function App() {
           ))}
         </Stack>
       </AppShell.Navbar>
-      <AppShell.Main>Main</AppShell.Main>
+      <AppShell.Main>This is the body</AppShell.Main>
     </AppShell>
   );
 }
